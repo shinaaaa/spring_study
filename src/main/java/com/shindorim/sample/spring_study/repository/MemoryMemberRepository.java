6ec977a677315e088adedfa960a17f8f60b6bd52
shinaaaa <shin029abc@gmail.com>
2020-12-24 21:04:12 +0900

    make domain and Repository

---
+++ src/main/java/com/shindorim/sample/spring_study/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package com.shindorim.sample.spring_study.repository;
+
+import com.shindorim.sample.spring_study.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequnce = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequnce);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
