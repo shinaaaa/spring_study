d39f414a2ca35a9efe7a0db0298a22ebf1c97f96
shinaaaa <shin029abc@gmail.com>
2021-01-21 00:28:27 +0900

    생성자 주입

---
+++ core/src/main/java/hello/core/discoount/RateDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discoount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        return (member.getGrade() == Grade.VIP ? price * discountPercent / 100 : 0);
+    }
+}
